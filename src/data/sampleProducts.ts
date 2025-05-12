08bcf1905f62fcb404f9c97a26589dbd13bac9ca
sadamahmed571 <sadam71589@gmail.com>
2025-05-12 08:57:47 +0300

    Add files via upload

---
+++ src/data/sampleProducts.ts
@@ -0,0 +1,7 @@
+
+import { Product } from '../components/product/ProductCard';
+
+// Helper to generate a random ID
+const generateId = () => Math.random().toString(36).substring(2, 15);
+
+export const sampleProducts: Product[] = [];
